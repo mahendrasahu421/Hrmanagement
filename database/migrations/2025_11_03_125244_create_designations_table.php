9538188c89b3bb3efc8bcc4b5be378b6d8359bca
Sneha-web1 <snehu0201@gmail.com>
2025-11-03 18:28:09 +0530

    completed department form

--- database/migrations/2025_11_03_125244_create_designations_table.php
+++ database/migrations/2025_11_03_125244_create_designations_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('designations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->string('designation_name');
+            $table->string('designation_code')->unique();
+            $table->boolean('status')->default(1)->comment('1 = Active, 0 = Inactive');
+            $table->text('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
